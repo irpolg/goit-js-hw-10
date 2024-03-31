99f24b1a16b89ed678c6ad01f12517adab93a097
irpolg <irpolg@ukr.net>
2024-04-01 01:30:51 +0300

    10hw-upd1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,19 +1,20 @@
 console.log("timer");
+import flatpickr from "flatpickr"; // Описаний в документації
+import "flatpickr/dist/flatpickr.min.css";   // Додатковий імпорт стилів - LMS
+import iziToast from "izitoast";   // Описаний у документації
+import "izitoast/dist/css/iziToast.min.css"; // Додатковий імпорт стилів
 
-// es modules are recommended, if available, especially for typescript
-// Описаний в документації
-import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
-//const flatpickr = require("flatpickr");
+const startBtn = document.querySelector("[data-start]");;
+const daysRef = document.querySelector('span[data-days]');
+const hoursRef = document.querySelector('span[data-hours]');
+const minutesRef = document.querySelector('span[data-minutes]');
+const secondsRef = document.querySelector('span[data-seconds]');
+const inputDateTime = document.querySelector('#datetime-picker');
 
+startBtn.disabled = true;
+startBtn.style.backgroundColor = "#CFCFCF";
+startBtn.style.color = "#989898";
 
-console.log(flatpickr);
-
-// Бібліотека очікує, що її ініціалізують на елементі 
-// input[type="text"], тому ми додали до HTML документа 
-// поле input#datetime-picker.
-// <input type="text" id="datetime-picker" />
 
 // Другим аргументом функції flatpickr(selector, options) 
 // можна передати необов'язковий об'єкт параметрів. 
@@ -21,13 +22,82 @@ console.log(flatpickr);
 // для виконання завдання. Розберися, за що відповідає 
 // кожна властивість у документації «Options» і 
 // використовуй його у своєму коді.
-
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-  console.log(selectedDates[0]);
-  },
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0].getTime() < new Date().getTime()) {
+            //alert("Виберіть майбутню дату!") // library
+            daysRef.textContent === "00";
+            hoursRef.textContent === "00";
+            minutesRef.textContent === "00";
+            secondsRef.textContent === "00";
+            iziToast.error({
+                title: 'Error',
+                titleColor: 'black',
+                backgroundColor: 'red',
+                message: 'Please choose a date in the future',
+                position: 'topRight',
+            });
+      } else {
+            startBtn.disabled = false;
+            startBtn.style.backgroundColor = "#4E75FF";
+            startBtn.style.color = "#FFFFFF";
+            startBtn.addEventListener("click", () => {    
+                startBtn.style.backgroundColor = "#CFCFCF";
+                startBtn.style.color = "#989898";
+                const userDate = setInterval(() => {
+                    const time = convertMs(selectedDates[0].getTime() - new Date().getTime());
+                    daysRef.textContent = addLeadingZero(time.days);
+                    //console.log(daysRef.textContent);
+                    hoursRef.textContent = addLeadingZero(time.hours);
+                    minutesRef.textContent = addLeadingZero(time.minutes);
+                    secondsRef.textContent = addLeadingZero(time.seconds);
+                    if (daysRef.textContent === "00" &&
+                        hoursRef.textContent === "00" &&
+                        minutesRef.textContent === "00" &&
+                        secondsRef.textContent === "00") {
+                        console.log(userDate);
+                        clearInterval(userDate);
+                    }
+                }, 1000);
+                startBtn.disabled = true;
+                inputDateTime.disabled = true;
+            })
+  }
+  }
 };
+
+flatpickr(inputDateTime, options);
+//flatpickr(selector, options);  - lms
+
+function addLeadingZero(value) {
+    //return `${n}`.padStart(2, '0');
+    return value.toString().padStart(2, '0');
+}
+
+
+// Для підрахунку значень використовуй готову 
+// функцію convertMs, де ms — різниця між 
+// кінцевою і поточною датою в мілісекундах.
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);   // Remaining days
+  const hours = Math.floor((ms % day) / hour); // Remaining hours
+  const minutes = Math.floor(((ms % day) % hour) / minute); // Remaining minutes
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second); // Remaining seconds
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log("convertMs: ", convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log("convertMs: ", convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log("convertMs: ", convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,37 @@
 console.log("snackbar");
+import iziToast from "izitoast";   // Описаний у документації
+import "izitoast/dist/css/iziToast.min.css"; // Додатковий імпорт стилів
+
+const form = document.querySelector(".form"); 
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const promise = new Promise((resolve, reject) => {
+        const { delay, state } = event.currentTarget.elements;
+        console.log({ delay: delay.value, state: state.value });
+
+        setTimeout(() => {
+            if (state.value === "fulfilled") {
+                iziToast.success({
+                    title: 'OK',
+                    titleColor: 'black',
+                    backgroundColor: '#59A10D',
+                    color: '#FFFFFF',
+                    message: `✅ Fulfilled promise in ${delay.value}ms`,
+                    position: 'topRight',
+             });
+            } else {
+                iziToast.error({
+                    title: 'Error',
+                    titleColor: 'black',
+                    backgroundColor: '#EF4040',
+                    color: '#FFFFFF',
+                    message: `❌ Rejected promise in ${delay.value}ms`,
+                    position: 'topRight',
+                });
+            };
+        }, delay);
+    })
+};
